8f4acfc57496edcadea01928f4cd9bf451158bc6
Parneet Kaur <kaurparneet152@gmail.com>
2021-07-29 03:50:21 -0400

    Assignment 3

--- app/src/main/java/com/example/sqliteapplication/ViewDataActivity.java
+++ app/src/main/java/com/example/sqliteapplication/ViewDataActivity.java
@@ -14,6 +14,7 @@ public class ViewDataActivity extends AppCompatActivity {
     TextView heading;
     ListView listView;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
